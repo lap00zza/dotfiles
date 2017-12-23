c68df5741bc0bfb7fb207e2d618bf5bf5ee97351
Jewel Mahanta <jewelmahanta@gmail.com>
2017-12-23 21:18:23 +0530

    remove npm init

--- project_setup/node.sh
+++ project_setup/node.sh
@@ -6,4 +6,4 @@ touch .gitignore && curl https://www.gitignore.io/api/node >> .gitignore
 touch .eslintrc.json && curl https://raw.githubusercontent.com/lap00zza/dotfiles/master/.eslintrc.json >> .eslintrc.json
 touch .editorconfig && curl https://raw.githubusercontent.com/lap00zza/dotfiles/master/.editorconfig >> .editorconfig
 git add README.md .gitignore .eslintrc.json .editorconfig
-npm init
+echo "All done. Remember to run npm --init and customize the other files if necessary."
