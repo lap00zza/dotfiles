1de182b4dc9c053f52a6960e8bd3cbdddab5eca1
Jewel Mahanta <jewelmahanta@gmail.com>
2017-12-23 22:16:03 +0530

    change location of eslintrc

--- project_setup/node.sh
+++ project_setup/node.sh
@@ -3,7 +3,7 @@
 git init
 touch README.md && echo "## Project Title" >> README.md
 touch .gitignore && curl https://www.gitignore.io/api/node >> .gitignore
-touch .eslintrc.json && curl https://raw.githubusercontent.com/lap00zza/dotfiles/master/.eslintrc.json >> .eslintrc.json
+touch .eslintrc.json && curl https://raw.githubusercontent.com/lap00zza/dotfiles/master/js/.eslintrc.json >> .eslintrc.json
 touch .editorconfig && curl https://raw.githubusercontent.com/lap00zza/dotfiles/master/.editorconfig >> .editorconfig
 git add README.md .gitignore .eslintrc.json .editorconfig
 echo "All done. Remember to run npm --init and customize the other files if necessary."
