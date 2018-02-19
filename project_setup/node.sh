932b3e7e269b8246e0fba1eb7b49abc1d21bd2da
Jewel Mahanta <jewelmahanta@gmail.com>
2018-02-19 21:11:59 +0530

    remove unnecessary touch

--- project_setup/node.sh
+++ project_setup/node.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 git init
-touch README.md && echo "## Project Title" >> README.md
-touch .gitignore && curl https://www.gitignore.io/api/node >> .gitignore
-touch .eslintrc.json && curl https://raw.githubusercontent.com/lap00zza/dotfiles/master/js/.eslintrc.json >> .eslintrc.json
-touch .editorconfig && curl https://raw.githubusercontent.com/lap00zza/dotfiles/master/.editorconfig >> .editorconfig
+echo "## Project Title" >> README.md
+curl https://www.gitignore.io/api/node >> .gitignore
+curl https://raw.githubusercontent.com/lap00zza/dotfiles/master/js/.eslintrc.json >> .eslintrc.json
+curl https://raw.githubusercontent.com/lap00zza/dotfiles/master/.editorconfig >> .editorconfig
 git add README.md .gitignore .eslintrc.json .editorconfig
 echo "All done. Remember to run npm --init and customize the other files if necessary."
