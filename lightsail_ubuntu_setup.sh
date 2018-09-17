c7280a813608a6fcf665aba3c8bd360407b021fb
Jewel Mahanta <jewelmahanta@gmail.com>
2018-09-17 13:04:05 +0530

    remove sshd_config line

--- lightsail_ubuntu_setup.sh
+++ lightsail_ubuntu_setup.sh
@@ -7,8 +7,12 @@ sudo apt upgrade -y
 
 # My default apps
 sudo apt install -y zsh htop nginx
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # To allow reverse ssh tunnel to bind to 0.0.0.0
-echo "\n# My Custom Edit\nGatewayPorts clientspecified" | sudo tee -a /etc/ssh/sshd_config
+# echo "\n# My Custom Edit\nGatewayPorts clientspecified" | sudo tee -a /etc/ssh/sshd_config
+
+# For the tasty oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # Cya!
-sudo reboot
+# sudo reboot
