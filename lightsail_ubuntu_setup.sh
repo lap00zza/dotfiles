4eb66325ff5a3de7994f7edcb7c947b674d40f67
Jewel Mahanta <jewelmahanta@gmail.com>
2018-09-20 15:57:27 +0530

    add nodejs

--- lightsail_ubuntu_setup.sh
+++ lightsail_ubuntu_setup.sh
@@ -8,6 +8,10 @@ sudo apt upgrade -y
 # My default apps
 sudo apt install -y zsh htop nginx
 
+# NodeJS
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
 # To allow reverse ssh tunnel to bind to 0.0.0.0
 # echo "\n# My Custom Edit\nGatewayPorts clientspecified" | sudo tee -a /etc/ssh/sshd_config
 
