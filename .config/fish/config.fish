b1f8c84119b21260825aafcd19369b9f89ff92bf
Jewel Mahanta <jewelmahanta@gmail.com>
2024-04-26 13:06:26 +0530

    added `url` in .gitconfig to enforce ssh

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,12 @@
 set -gx LANG "en_IN.utf-8"
 
 # Make FZF use FD (https://github.com/sharkdp/fd)
-set -gx FZF_DEFAULT_COMMAND "fd --type file --follow --hidden --color=always"
-set -gx FZF_DEFAULT_OPTS "--ansi"
-set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+set -gx FZF_DEFAULT_COMMAND fd --type file --follow --hidden --color=always --max-depth 5
+set -gx FZF_DEFAULT_OPTS --ansi
+set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+
+# fzf.fish options
+set -gx fzf_fd_opts --follow --hidden --max-depth 5
 
 # remove greeting
 set -U fish_greeting
@@ -33,25 +36,6 @@ function devenv -d "Launch TMUX with my preferred layout containing btop, ping, 
 
     tmux selectp -t 3
     tmux attach -t "devenv"
-end
-
-# This function pushes my changes to my Github remote via SSH. 
-#
-# Q> Why not just use `git push origin`?
-# A> Because github requires access tokens and I dislike copy pasting it a lot. Besides,
-#    SSH is easier.
-# Q> Why not add ssh as a remote?
-# A> Because I don't like to pollute the remote list.
-function ghp_ssh -d "Push local changes to gh remote via SSH"
-    # parse username
-    set -l gh_username (git remote -v | grep "fetch" | cut -d "/" -f4)
-    # parse repository
-    set -l gh_repo (git remote -v | grep "fetch" | cut -d "/" -f5 | cut -d " " -f1 | cut -d "." -f1)
-    # 
-    set -l repo_url "git@github.com:$gh_username/$gh_repo"
-    echo "REPO URL (SSH) => $repo_url"
-    git push $repo_url
-    git pull
 end
 
 if status is-interactive
